2abdb3e3ef7242a18046ece347ef41de1cff93c4
mikee47 <mike@sillyhouse.net>
2021-11-23 21:42:17 +0000

    review

--- README.rst
+++ README.rst
@@ -1,45 +1,48 @@
-JerryScript integration component.
+JerryScript
+===========
+
+.. highlight:: bash
 
 Status
-======
+------
+
 Currently we use version v2.2 from JerryScript
 
 Update
-======
+------
 
-In order to update the version of JerryScript two tasks have to be executed
+In order to update the version of JerryScript two tasks have to be executed:
+
 - in this repository to update the submodule version of JerryScript to point to the new version
 - in https://github.com/attachix/jerryscript repository that same version has to be merged too.
 
 Once that is done we need to build completely from scratch a new docker container.
 
 Dockerfile
-===========
+----------
+
 Up to now we have good results with creating a pure JavaScript code using the latest sdk.
 There is a docker container that creates the needed environment.
-It can be built and run  with following commands
+It can be built and run with the following commands::
 
     docker build -f components/jerryscript/Dockerfile -t jerryscript-ems .
     docker run -it jerryscript-ems
 
-Inside the container a new jerryscript snapshot compiler can be built by calling
+Inside the container a new jerryscript snapshot compiler can be built by calling::
 
     node /jerryscript/build/bin/jsc.js -o /tmp/test.js.snap /tmp/test.js
 
-Where `test.js.snap` is the output file and /tmp/test.js is an existing JavaScript file.
+Where ``test.js.snap`` is the output file and ``/tmp/test.js`` is an existing JavaScript file.
 
-To update the already precompiled jsc.js in `web/dev` you can do the following:
+To update the already precompiled jsc.js in ``web/dev`` you can do the following::
 
     docker run -v $(pwd)/web/:/web -it jerryscript-ems
 
-And run the following commands inside the container
+And run the following commands inside the container::
 
     cp -r /jerryscript/build/bin/jsc.* /web/dev
 
 That will require also precompilation of the main.js file.
-Inside the container this can be done with the following command.
+Inside the container this can be done with the following command::
 
     node /jerryscript/build/bin/jsc.js -o /web/build/main.js.snap /web/dev/main.js
-
-
-
--- component.mk
+++ component.mk
@@ -1,19 +1,9 @@
 # JerryScript 
-COMPONENT_NAME := jerryscript
 COMPONENT_SUBMODULES := jerryscript
 
-JERRY_CORE := $(COMPONENT_PATH)/jerryscript/jerry-core/
-JERRY_INCDIRS := jerryscript $(JERRY_CORE) $(call ListAllSubDirs, $(JERRY_CORE))
-COMPONENT_SRCDIRS := $(call ListAllSubDirs, $(JERRY_CORE))
-COMPONENT_INCDIRS := $(JERRY_INCDIRS)
-INCDIR = $(JERRY_INCDIRS)
-
-COMPONENT_CFLAGS := -std=c99 -D__TARGET_ESP8266
-COMPONENT_CFLAGS += -Wall -Os -g -O2 -Wpointer-arith -Wno-implicit-function-declaration -Wl,-EL -fno-inline-functions -nostdlib -D__ets__ -DICACHE_FLASH 
-
-ifeq ($(SMING_ARCH),Esp8266)
-	COMPONENT_CFLAGS += -mlongcalls  -mno-text-section-literals
-endif
+JERRYSCRIPT_CORE := $(COMPONENT_PATH)/jerryscript/jerry-core
+COMPONENT_SRCDIRS := $(call ListAllSubDirs,$(JERRYSCRIPT_CORE))
+COMPONENT_INCDIRS := jerryscript $(JERRYSCRIPT_CORE) $(COMPONENT_SRCDIRS)
 
 # The size of the JerryScript engine heap. The size in kilobytes is allocated once and defined at compile time.
 COMPONENT_VARS := JERRY_GLOBAL_HEAP_SIZE
@@ -23,22 +13,17 @@ JERRY_GLOBAL_HEAP_SIZE ?= 1
 COMPONENT_VARS += JERRY_COMPACT_PROFILE  
 JERRY_COMPACT_PROFILE ?= 1
 
-# COMPONENT_CFLAGS += -fno-builtin -fno-stack-protector -g -gdwarf-4 -Wall -Werror=all -Wextra -Werror=extra -Wformat-nonliteral -Werror=format-nonliteral -Winit-self -Werror=init-self -Wconversion -Werror=conversion -Wsign-conversion -Werror=sign-conversion -Wformat-security -Werror=format-security -Wmissing-declarations -Werror=missing-declarations -Wno-stack-protector -Wno-attributes -Wlogical-op -Werror=logical-op  -D__attr_always_inline___= -Wl,-EL -fno-inline-functions -ffunction-sections -fdata-sections -mlongcalls -mtext-section-literals -mno-serialize-volatile -pedantic -Os
-ifeq ($(JERRY_COMPACT_PROFILE), 1)
-   COMPONENT_CFLAGS += -DJERRY_BUILTINS=0 \
-              		     -DJERRY_ES2015=0 \
-              		     -DJERRY_UNICODE_CASE_CONVERSION=0
+ifeq ($(JERRY_COMPACT_PROFILE),1)
+	COMPONENT_CFLAGS += \
+		-DJERRY_BUILTINS=0 \
+		-DJERRY_ES2015=0 \
+		-DJERRY_UNICODE_CASE_CONVERSION=0
 endif
 
-COMPONENT_CFLAGS += -DJERRY_NUMBER_TYPE_FLOAT64=0 -DJERRY_GLOBAL_HEAP_SIZE=$(JERRY_GLOBAL_HEAP_SIZE) \
-              		  -DJERRY_LCACHE=0  \
-              		  -DJERRY_PARSER=0 -DJERRY_SNAPSHOT_EXEC=1 -DJERRY_NDEBUG -D_DEFAULT_SOURCE \
-              		  -D__TARGET_ESP8266 -D__attr_always_inline___=
-
-MFORCE32 := $(shell $(CC) --help=target | grep mforce-l32)
-
-ifneq ($(MFORCE32),)
-    # Your compiler supports the -mforce-l32 flag which means that 
-    # constants can be placed in ROM to free additional RAM
-    COMPONENT_CFLAGS += -DJERRY_ATTR_CONST_DATA="__attribute__((aligned(4))) __attribute__((section(\".irom.text\")))" -mforce-l32 
-endif
+COMPONENT_CFLAGS += \
+	-DJERRY_NUMBER_TYPE_FLOAT64=0 \
+	-DJERRY_GLOBAL_HEAP_SIZE=$(JERRY_GLOBAL_HEAP_SIZE) \
+	-DJERRY_LCACHE=0  \
+	-DJERRY_PARSER=0 \
+	-DJERRY_SNAPSHOT_EXEC=1 \
+	-DJERRY_NDEBUG
